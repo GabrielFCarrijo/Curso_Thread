0233c5435282ed59e22d5a58898d3ff19c24c027
GabrielCarrijoF <carrijogit2003@gmail.com>
2022-10-31 23:12:15 -0300

    refactor: esperando limpeza

--- CursoThread/src/banheiro/Banheiro.java
+++ CursoThread/src/banheiro/Banheiro.java
@@ -2,6 +2,8 @@ package banheiro;
 
 public class Banheiro {
 
+    private boolean banheiroSujo = true;
+
     public void fazNumero1() {
 
         String nome = Thread.currentThread().getName();
@@ -9,20 +11,37 @@ public class Banheiro {
 
         synchronized (this) {
             System.out.println("Entro");
+
+            if (banheiroSujo) {
+                esperaLaFora(nome);
+            }
+
             System.out.println(nome + " Fez pips");
             System.out.println("Saiu");
         }
     }
 
+    private void esperaLaFora(String nome) {
+        System.out.println(nome + "eca, banheiro sujo");
+
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void fazNumero2() {
-
-
 
         String nome = Thread.currentThread().getName();
         System.out.println(nome + " Batendo na porta");
 
         synchronized (this) {
             System.out.println("Entro");
+
+            if (banheiroSujo){
+                esperaLaFora(nome);
+            }
 
             try {
                 Thread.sleep(1000);
